38368e93fda8961548103d0a8c9796aa98a5c29a
flavius <kete102@gmail.com>
2023-12-12 15:59:56 +0100

    Finishing layout

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,12 @@
-console.log("hola funcionas?");
+import "../css/index.css";
+
+const wrapper = document.querySelector(".wrapper");
+const backBtn = document.querySelector(".back-btn");
+const menuBtn = document.querySelector(".menu-btn");
+
+const toggleScreen = () => {
+  wrapper.classList.toggle("show-category");
+};
+
+menuBtn.addEventListener("click", toggleScreen);
+backBtn.addEventListener("click", toggleScreen);
